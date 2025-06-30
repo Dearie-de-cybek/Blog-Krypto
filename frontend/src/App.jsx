70a0c7a989777f188dd1195cf9f8152b930bd1ba
Dearie <eburujacintha@gmail.com>
2025-06-30 20:58:01 +0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+};
+
+export default App;
