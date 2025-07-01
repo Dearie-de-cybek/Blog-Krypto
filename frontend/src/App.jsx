4a274cfa5e83f3ed5414a173295671a0b44effdb
Dearie <eburujacintha@gmail.com>
2025-07-01 09:33:26 +0400

    added header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,11 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
