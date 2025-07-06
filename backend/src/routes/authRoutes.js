0c60ebe9ab73d540980c9f51e60b030d89c47f3f
Dearie <eburujacintha@gmail.com>
2025-07-06 21:11:12 +0400

    made changes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -8,6 +8,7 @@ router.post('/login', login);
 router.post('/logout', logout);
 router.get('/me', protect, getMe);
 
+// Create initial admin (remove this route after creating admin or add protection)
 router.post('/create-admin', createAdmin);
 
 module.exports = router;
