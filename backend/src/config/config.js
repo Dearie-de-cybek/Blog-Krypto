cdebcedc54e47b568903cbbcaaa3089a7afd4fa1
Dearie <eburujacintha@gmail.com>
2025-07-06 20:53:08 +0400

    added new file

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || 'sdcsdcdc32ry38y9dpnp23i3892te832tp9e230n',
+  JWT_SECRET: process.env.JWT_SECRET || 'gvivhsdsjvnhdbvfsjhdfbgskdghbfjdbvshdfbvdjvhndfkjsvhbhdfvbdfjvdsjbvhfdbvhd',
   JWT_EXPIRE: process.env.JWT_EXPIRE || '30d',
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 5000,
   NODE_ENV: process.env.NODE_ENV || 'development'
 };
