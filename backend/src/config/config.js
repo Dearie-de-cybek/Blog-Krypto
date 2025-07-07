af8239b99d290d75b6c39a3ad02af8ecbdb6e252
Dearie <eburujacintha@gmail.com>
2025-07-07 08:26:31 +0400

    made changes

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'gvivhsdsjvnhdbvfsjhdfbgskdghbfjdbvshdfbvdjvhndfkjsvhbhdfvbdfjvdsjbvhfdbvhd',
   JWT_EXPIRE: process.env.JWT_EXPIRE || '30d',
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development'
 };
