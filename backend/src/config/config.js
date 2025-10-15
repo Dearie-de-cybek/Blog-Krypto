e93191b198184055a3c796f798b83e4f0d4b9e03
Eburu Dearie <65720363+Dearie-de-cybek@users.noreply.github.com>
2025-10-15 06:14:48 +0400

    Delete backend directory

--- backend/src/config/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  JWT_SECRET: process.env.JWT_SECRET || 'gvivhsdsjvnhdbvfsjhdfbgskdghbfjdbvshdfbvdjvhndfkjsvhbhdfvbdfjvdsjbvhfdbvhd',
-  JWT_EXPIRE: process.env.JWT_EXPIRE || '30d',
-  PORT: process.env.PORT || 8080,
-  NODE_ENV: process.env.NODE_ENV || 'development'
-};
